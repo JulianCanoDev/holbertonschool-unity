8071dcd8693690b9c5e120369ceff9f7c278ecd3
Julian <juliancano.dev@gmail.com>
2021-01-08 12:06:50 -0800

    Task 0

---
+++ 0x04-unity_publishing/Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    private float degree = 45;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(degree * Time.deltaTime, 0, 0);
+    }
+}
