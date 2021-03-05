f7eeaa913575bc26dd282067bb8252d5924abcea
Julian <juliancano.dev@gmail.com>
2021-03-04 22:22:25 -0500

    Task 3

--- 0x07-unity-animation/Assets/Scripts/CharacterAnim.cs
+++ 0x07-unity-animation/Assets/Scripts/CharacterAnim.cs
@@ -22,5 +22,10 @@ public class CharacterAnim : MonoBehaviour
         {
             anim.SetBool("isRunning", false);
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("jump");
+        }
     }
 }
