677eb77de69c53f714f296376e9fc6794ebf3087
Julian <juliancano.dev@gmail.com>
2021-03-17 22:19:09 -0500

    Tasks 1 - 2

--- 0x08-unity-audio/Assets/Scripts/CutsceneController.cs
+++ 0x08-unity-audio/Assets/Scripts/CutsceneController.cs
@@ -7,18 +7,6 @@ public class CutsceneController : MonoBehaviour
     public GameObject MainCamera;
     public GameObject Player;
     public GameObject TimerCanvas;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void camTransition()
     {
         MainCamera.SetActive(true);
